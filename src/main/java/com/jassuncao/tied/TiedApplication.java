5cd13225decdd71e931006dfacda85ed1a135088
Jônathas Assunção <jaa020399@gmail.com>
2021-09-07 10:43:51 -0300

    feat: Created project

---
+++ src/main/java/com/jassuncao/tied/TiedApplication.java
@@ -0,0 +1,13 @@
+package com.jassuncao.tied;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jassuncao/tied/TiedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jassuncao.tied;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
