9558726248b6bdc99903070867bb9206c2ba6d0c
Jônathas Assunção <jaa020399@gmail.com>
2021-09-17 00:24:03 -0300

    fix: Corrigido testes

--- src/main/resources/templates/entity.java
+++ src/main/resources/templates/entity.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Optional;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Lob;
